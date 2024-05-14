f16d85373f672c5f607297eb653e8afb4fde64ec
AnzeFric <anzeifric@gmail.com>
2024-05-14 22:34:53 +0200

    Added statistics controller logic

--- controllers/statisticController.js
+++ controllers/statisticController.js
@@ -3,6 +3,25 @@
  */
 
 // Display all statistics
-exports.display = async (req, res) => {
-    
+exports.display = async (req, res, supabase) => {
+    try {
+        const { id } = req.params;
+        const { data, error } = await supabase
+            .from('STATISTIC')
+            .select('*')
+            .eq('TK_user', id)
+            .single();
+
+        if (error) {
+            return res.status(500).json({ error: 'Error when getting statistics' });
+        }
+
+        if (data.length === 0) {
+            return res.status(404).json({ error: 'Statistics not found' });
+        }
+
+        res.json({ data });
+    } catch (error) {
+        throw error;
+    }
 };

9c5a8d5957a8e82bdba598c264e2255892e19479
AnzeFric <120261988+AnzeFric@users.noreply.github.com>
2024-05-29 09:10:17 +0200

    Changed paths, that don't need authentication

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -10,7 +10,7 @@ const secretKey = process.env.JWT_SECRET || 'work hard';
 // Middleware function for authentication
 const authMiddleware = (req, res, next) => {
   // Check if the request path is for authentication or registration
-  if (req.path === '/api/users/login' || req.path === '/api/users/register') {
+  if (req.path === '/users/login' || req.path === '/users/register') {
     // Allow access to authentication and registration routes
     return next();
   }
